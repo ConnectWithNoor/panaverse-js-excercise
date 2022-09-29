e35d88671dd7167f4b3941eba324eb29fa427309
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 10:56:09 +0500

    Q7 completed

---
+++ Q7/index.js
@@ -0,0 +1,9 @@
+const addition = 4 + 4;
+const substraction = 8 - 4;
+const multiplication = 4 * 2;
+const division = 16 / 2;
+
+console.log('addition', addition);
+console.log('substraction', substraction);
+console.log('multiplication', multiplication);
+console.log('division', division);

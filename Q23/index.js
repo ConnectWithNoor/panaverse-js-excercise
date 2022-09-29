f05f7e2877c6b0eae3f16c193c595c9471e0b670
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 12:55:01 +0500

    Q23 completed

---
+++ Q23/index.js
@@ -0,0 +1,25 @@
+const car = 'Toyota';
+const country = 'Pakistan';
+const langauge = 'Urdu';
+const bike = 'Honda';
+const mobile = 'Samsung';
+const laptop = 'Dell';
+const date = 29;
+const day = 'Thursday';
+const course = 'Panaverse';
+const teacher = ['Sir Zia', 'Sir Daniyal'];
+
+// the falsy comparision that would result in false
+console.log(car === 'KIA');
+console.log(country === 'India');
+console.log(langauge === 'English');
+console.log(course === 'Blockchain');
+console.log(teacher.length === 1);
+
+// the truthy comparision that would result in true
+
+console.log(bike === 'Honda');
+console.log(mobile === 'Samsung');
+console.log(laptop === 'Dell');
+console.log(date === 29);
+console.log(day === 'Thursday');

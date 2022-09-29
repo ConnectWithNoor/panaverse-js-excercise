97dc6f2adce2151161352386e8cb85d14ea6e172
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 12:31:28 +0500

    Q19 completed

---
+++ Q19/index.js
@@ -0,0 +1,21 @@
+// Q14
+let inviteList = ['Wahab', 'Naureen', 'Roha', 'Arham'];
+console.log(`Number of people invited in Q14: ${inviteList.length}`);
+
+// Q15
+inviteList = ['Wahab', 'Hamza', 'Roha', 'Hassan'];
+console.log(`Number of people invited in Q15: ${inviteList.length}`);
+
+// Q16
+inviteList.push('Abdul Rehman', 'Shafi', 'Ahmed');
+console.log(`Number of people invited in Q16: ${inviteList.length}`);
+
+// Q17
+while (inviteList.length > 2) {
+  inviteList.pop();
+}
+console.log(`Number of people invited in Q17: ${inviteList.length}`);
+
+// Q18
+const places = ['Venice', 'Istanbul', 'Paris', 'San Francisco', 'Rome'];
+console.log(`Number of people invited in Q18: ${inviteList.length}`);

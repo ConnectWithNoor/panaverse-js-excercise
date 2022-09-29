480e233a24e1d397f108599c44cf31c2652cba2c
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 13:25:03 +0500

    Q29 completed

---
+++ Q29/index.js
@@ -0,0 +1,20 @@
+const favorite_fruits = ['apple', 'banana', 'grape'];
+
+if (favorite_fruits.includes('banana')) {
+  console.log('You really liked banana');
+}
+
+if (favorite_fruits.includes('apple')) {
+  console.log('You really liked apple');
+}
+
+if (favorite_fruits.includes('grape')) {
+  console.log('You really liked grape');
+}
+
+if (favorite_fruits.includes('mango')) {
+  console.log('You really liked mango');
+}
+if (favorite_fruits.includes('pineapple')) {
+  console.log('You really liked pineapple');
+}

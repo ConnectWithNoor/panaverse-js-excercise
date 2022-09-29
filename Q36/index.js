717cf1ec254251a9cda83338cdc6bcac9354fb00
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 14:25:54 +0500

    Q36 completed

---
+++ Q36/index.js
@@ -0,0 +1,5 @@
+const makeTshirts = (size, text) => {
+  return `the size of the shirt should be ${size} and the text message printed on it must be ${text}`;
+};
+
+console.log(makeTshirts('M', 'Hala Madrid'));

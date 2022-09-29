085ac2373f2a131b68a6189d746fd02f83b5d6ef
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 13:30:37 +0500

    Q30 completed

---
+++ Q30/index.js
@@ -0,0 +1,9 @@
+const usernames = ['Noor', 'Muzammil', 'Mudassir', 'Admin', 'Wahab'];
+
+usernames.forEach((user) => {
+  if (user === 'Admin') {
+    console.log(`Hello ${user}, would you like to see a status report?`);
+  } else {
+    console.log(`Hello ${user}, thank you for logging in again.`);
+  }
+});

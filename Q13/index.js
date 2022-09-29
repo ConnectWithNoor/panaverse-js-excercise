e55143ea45c5aa2ba077c03cfb9686a785ff9287
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 11:41:12 +0500

    Q13 completed

---
+++ Q13/index.js
@@ -0,0 +1,10 @@
+const vehicles = ['bike', 'car', 'ship', 'airplane'];
+
+console.log(`Always use helmet when riding a ${vehicles[0]}`);
+console.log(`${vehicles[1]}s are comfirtable but consume more fuel`);
+console.log(
+  `Cargos and shipments worldwide are carried by ${vehicles[2]} at large scale`
+);
+console.log(
+  `6.84 of every 100,000 flight hours yields an ${vehicles[3]} crash`
+);

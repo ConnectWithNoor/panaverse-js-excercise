8fad8bc7dfd2ca471cb86d8f79120425abe4aa31
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 11:00:27 +0500

    Q9 completed

---
+++ Q8/index.js
@@ -0,0 +1,9 @@
+// didn't understand the question propety. I did what I understood. Plese pardon if it's wrong.
+
+const message = `---------------------------------
+console.log(${5 + 3})
+---------------------------------`;
+
+for (let i = 0; i < 4; i++) {
+  console.log(message);
+}

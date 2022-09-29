1e7773d9844e8b64326372346b3b713cc9f716ba
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 14:12:23 +0500

    Q34 completed

---
+++ Q34/index.js
@@ -0,0 +1,7 @@
+const pizzaList = ['fajita', 'tikka', 'italian', 'supreme', 'bihari'];
+
+pizzaList.forEach((pizzaName) => console.log(`I like ${pizzaName} pizza`));
+
+console.log(
+  'My love for pizza runs deep. I like pizza in any form. From gourmet to frozen and deep dish to thin & crispy, I love it ALL. I really enjoy making my own pizza.'
+);

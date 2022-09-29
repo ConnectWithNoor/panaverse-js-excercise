56999d4092fa451c32d9e1515daf8556a05e1f8c
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 11:44:29 +0500

    Q14 completed

---
+++ Q14/index.js
@@ -0,0 +1,14 @@
+const inviteList = ['Wahab', 'Naureen', 'Roha', 'Arham'];
+
+console.log(
+  `Hey ${inviteList[0]}, I'm organizing a dinner party tonight at my place, you are invited. It'll start sharp at 9. Please be on time.`
+);
+console.log(
+  `Hey ${inviteList[1]}, I'm organizing a dinner party tonight at my place, you are invited. It'll start sharp at 9. Please be on time.`
+);
+console.log(
+  `Hey ${inviteList[2]}, I'm organizing a dinner party tonight at my place, you are invited. It'll start sharp at 9. Please be on time.`
+);
+console.log(
+  `Hey ${inviteList[3]}, I'm organizing a dinner party tonight at my place, you are invited. It'll start sharp at 9. Please be on time.`
+);

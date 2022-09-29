30196aea8430421b491a658e18331660fdc93dc4
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 11:08:17 +0500

    Q12 completed

---
+++ Q12/index.js
@@ -0,0 +1,6 @@
+const friendsName = ['Wahab', 'Naureen', 'Roha', 'Arham'];
+
+console.log(`Hey ${friendsName[0]}, how are you doing today?`);
+console.log(`Hey ${friendsName[1]}, how are you doing today?`);
+console.log(`Hey ${friendsName[2]}, how are you doing today?`);
+console.log(`Hey ${friendsName[3]}, how are you doing today?`);

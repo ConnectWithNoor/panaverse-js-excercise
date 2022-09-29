c357a17b516ba39e7ba33d9b2bb33cc777307e61
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 11:05:00 +0500

    Q10 completed

---
+++ Q10/index.js
@@ -0,0 +1,26 @@
+// Q7
+
+// The problem was to perform the maths operation that are addition, substraction, multiplication and division
+// Noor Muhammad - 29 Sep 2022
+
+const addition = 4 + 4;
+const substraction = 12 - 4;
+const multiplication = 4 * 2;
+const division = 16 / 2;
+
+console.log('addition', addition);
+console.log('substraction', substraction);
+console.log('multiplication', multiplication);
+console.log('division', division);
+
+// Q6
+
+// The problem was to take a user's name as input which has whitespaces in the beginnering and the end
+// of the string, we have to remove the whitespaces and print the output.
+// Noor Muhammad - 29 Sep 2022
+
+const username = '\t \t \t Noor Muhammad \n \n';
+const trimmedUsername = username.trim();
+
+console.log(`Before: ${username}`);
+console.log(`After: ${trimmedUsername}`);

bce1119c42abe45db6e43cbc6013052e2d5bf1a0
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 13:13:34 +0500

    Q25 completed

--- Q25/index.js
+++ Q25/index.js
@@ -6,10 +6,6 @@ const alienColor = colors[Math.floor(Math.random() * 3)];
 if (alienColor === 'green') {
   points += 5;
   console.log("You've earned 5 juicy points");
-} else if (alienColor === 'yellow') {
-  points += 10;
-  console.log("You've earned 10 juicy points");
-} else if (alienColor === 'red') {
-  points += 15;
-  console.log("You've earned 15 juicy points");
+} else {
+  console.log('Oops, The alien color is', alienColor, 'Better luck next time.');
 }

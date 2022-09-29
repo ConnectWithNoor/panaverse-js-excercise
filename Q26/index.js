e60218ced0912c220ee98f93d98919bf653755c6
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 13:15:30 +0500

    Q26 completed

--- Q26/index.js
+++ Q26/index.js
@@ -6,10 +6,7 @@ const alienColor = colors[Math.floor(Math.random() * 3)];
 if (alienColor === 'green') {
   points += 5;
   console.log("You've earned 5 juicy points");
-} else if (alienColor === 'yellow') {
+} else {
   points += 10;
   console.log("You've earned 10 juicy points");
-} else if (alienColor === 'red') {
-  points += 15;
-  console.log("You've earned 15 juicy points");
 }

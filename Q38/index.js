bafe008407f3ac96bac9403a6c4ddbaadac44449
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 15:23:46 +0500

    Q38 completed

---
+++ Q38/index.js
@@ -0,0 +1,7 @@
+const describeCity = (city, country = 'Pakistan') => {
+  console.log(`${city} is in ${country}`);
+};
+
+describeCity('Mumbai', 'India');
+describeCity('Islamabad');
+describeCity('Karachi');

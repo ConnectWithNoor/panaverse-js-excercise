fe3e2f53a6ebfcce8ecfb85b4481a968e61c9e92
Noor Muhammad <connectwithnoor1@gmail.com>
2022-09-29 15:30:04 +0500

    Q40 completed

---
+++ Q40/index.js
@@ -0,0 +1,10 @@
+const makeAlbum = (artist, album) => {
+  return {
+    artist,
+    album,
+  };
+};
+
+console.log(makeAlbum('A.R Rehman', 'Jay Ho'));
+console.log(makeAlbum('A.R Rehman', 'Dil se ray'));
+console.log(makeAlbum('Carolina', 'Taylor swift'));
